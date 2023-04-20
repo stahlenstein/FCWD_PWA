29627ffeb551e836583caf7a98bd0bba2d5581a0
Main-FCWD <126495756+Main-FCWD@users.noreply.github.com>
2023-04-20 16:46:40 -0400

    New Login Page

--- waterMgmt/src/App.vue
+++ waterMgmt/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
     <header>
-      <span>Project Neptune</span>
+      <span></span>
     </header>
     <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
       <router-view></router-view>
     </main>
   </div>
@@ -25,19 +24,19 @@ body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #35495e2d;
 }
 
 main {
   text-align: center;
-  margin-top: 40px;
+  margin-top: 0px;
 }
 
 header {
   margin: 0;
-  height: 56px;
+  height: 0px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
+  background-color: #ffffff;
   color: #ffffff;
 }
 
--- waterMgmt/src/components/Hello.vue
+++ waterMgmt/src/components/Hello.vue
@@ -1,39 +1,165 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2></h2>
+<div id="app">
 
-  </div>
+<div class="login-page">
+   <transition name="fade">
+      <div v-if="!registerActive" class="wallpaper-login"></div>
+   </transition>
+   <div class="wallpaper-register"></div>
+
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
+            <div v-if="!registerActive" class="card login" v-bind:class="{ error: emptyFields }">
+               <h1>Sign In</h1>
+               <form class="form-group">
+                  <input v-model="emailLogin" type="email" class="form-control" placeholder="Email" required>
+                  <input v-model="passwordLogin" type="password" class="form-control" placeholder="Password" required>
+                  <input type='submit' class="btn btn-primary" value="Submit" @click="doLogin">
+                  <p><a href="#" @click="registerActive = !registerActive, emptyFields = false"></a>
+                  </p>
+                  <p><a href="#"></a></p>
+               </form>
+            </div>
+
+            <div v-else class="card register" v-bind:class="{ error: emptyFields }">
+            </div>
+         </div>
+      </div>
+
+   </div>
+</div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Project Neptune'
+  name: 'app',
+  data: {
+    registerActive: false,
+    emailLogin: '',
+    passwordLogin: '',
+    emailReg: '',
+    passwordReg: '',
+    confirmReg: '',
+    emptyFields: false
+  },
+  methods: {
+    doLogin () {
+      if (this.emailLogin === '' || this.passwordLogin === '') {
+        this.emptyFields = true
+      } else {
+        alert('You are now logged in')
+      }
+    },
+    doRegister () {
+      if (this.emailReg === '' || this.passwordReg === '' || this.confirmReg === '') {
+        this.emptyFields = true
+      } else {
+        alert('You are now registered')
+      }
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style>
-h1, h2 {
-  font-weight: normal;
+.container {
+    z-index: 1;
+    position: absolute;
+    align-self: center;
+    left:5vw;
+    width: 15em;
+    height: 20em;
+    background-color: white;
+    border:black thin;
+    border-radius: 0.25rem;
+    box-shadow: 5px 5px 30px black;
 }
-
-ul {
-  list-style-type: none;
-  padding: 0;
+.card h1 {
+text-align: left;
+font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
-
-li {
-  display: inline-block;
-  margin: 0 10px;
+.row {
+    position: absolute;
 }
-
-a {
-  color: #35495E;
+.p {
+  line-height: 1rem;
+}
+.card {
+  padding: 20px;
+}
+.form-group input {
+  margin-bottom: 10px;
+}
+.btn-primary {
+    display: block;
+    background-color: #007bff;
+    border: #007bff;
+    color: white;
+    align-self: center;
+    border-radius: 0.25rem;
+    font: 1em sans-serif;
+    font-weight: 600;
+    text-align: center;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 16px;
+    height: 30px;
+    width: 70px;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+}
+.login-page {
+  justify-content: center;
+  display: flex;
+  height: 100vh;
+}
+.login-page .wallpaper-login {
+  background: url(https://images.unsplash.com/photo-1615800634551-745f94ffa49d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=scale&w=2242&q=100&orient=8) no-repeat center center;
+  transform: rotate(-180deg);
+  background-size: cover;
+  height: 100%;
+  position: relative;
+  width: 100%;
+  z-index: 0;
+}
+.login-page .fade-enter-active, .login-page .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.login-page .fade-enter, .login-page .fade-leave-to {
+  opacity: 0;
+}
+.login-page .wallpaper-register {
+  background: url() no-repeat center center;
+  background-size: cover;
+  height: 100%;
+  position: absolute;
+  width: 100%;
+  z-index: -1;
+}
+.login-page h1 {
+  margin-bottom: 1.5rem;
+  color: black;
+}
+.error {
+  animation-name: errorShake;
+  animation-duration: 0.3s;
+}
+@keyframes errorShake {
+  0% {
+    transform: translateX(-25px);
+  }
+  25% {
+    transform: translateX(25px);
+  }
+  50% {
+    transform: translateX(-25px);
+  }
+  75% {
+    transform: translateX(25px);
+  }
+  100% {
+    transform: translateX(0);
+  }
 }
 </style>
